ef3ea1647a014f74e30da3428d199a51c1fe6045
= <=>
2024-12-06 20:39:27 +0100

    changed base path for views

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ const MemoryStore = require('memorystore')(session);
 const app = express();
 
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname + '../public')));
 app.set('view engine', 'ejs');
 app.use(session({
   secret: 'keyboard cat', // da cambiare a sicura tipo
